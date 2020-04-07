3df4f179a25f39425559b8d9f6c0e84da3a3eb32
Rensy <rensy.aikara@gmail.com>
2020-04-07 14:30:56 -0500

    added code to js file, to work with food API linking temperature

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("#button-addon2").on("click", function () {
         var cityName = $("#cityInput").val();
         apicallFunction(cityName);
+       
     })
 
     function apicallFunction(cityName) {
@@ -20,9 +21,60 @@ $(document).ready(function () {
             var tempC = response.main.temp;
             var tempF = (((tempC- 273.15) * 1.80) + 32).toFixed(2);
             console.log(tempF);
-            $("#temp").text("Temperature: " + tempF);
-        
+            $("#temp").text("Temperature: " + tempF +"F");
+            foodapicall(tempF);
 
         });
     }
-});
+    
+     function foodapicall(tempF){
+         if(tempF < 20){
+            var array =["52846", "52814", "52867", "52812", "52806"];
+            var indexValue = Math.floor(Math.random() *(5));  
+            var recipeChoice = array[indexValue];     
+             console.log("index value: " +recipeChoice);    
+         }
+
+         else  if(tempF >= 20 && tempF < 40){
+            var array =["52813", "52844", "52993", "52805", "52810"];
+            var indexValue = Math.floor(Math.random() *(5));
+            var recipeChoice = array[indexValue];   
+            console.log("index value: " +recipeChoice);   
+         }
+
+         else  if(tempF >= 40 && tempF < 60){
+            var array =["52959", "52773", "52803", "52796", "52807"];
+            var indexValue = Math.floor(Math.random() *(5));
+            var recipeChoice = array[indexValue];     
+             console.log("index value: " +recipeChoice);     
+         }
+
+         else  if(tempF >= 60 && tempF < 80){
+            var array =["52994", "53010", "52997", "52832", "52816"];
+            var indexValue = Math.floor(Math.random() *(5));
+            var recipeChoice = array[indexValue];    
+             console.log("index value: " +recipeChoice);     
+         }
+
+         else if(tempF >= 80){
+            var array =["52819", "52777", "52960", "52937", "52852"];
+            var indexValue = Math.floor(Math.random() *(5));
+            var recipeChoice = array[indexValue];    
+            console.log("index value: " +recipeChoice);     
+         }
+     }
+
+     function recipeapifunction(recipeChoice){
+      var queryURL= "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recipeChoice;   
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function (response) {
+
+        });
+
+     }
+
+
+    });
+    
